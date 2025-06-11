58bccf0524be6aeb0d3a6cdd177afbbd4b1cf593
Febyanti Eirene <eirene.febyanti@gmail.com>
2025-06-11 09:44:46 +0700

    nyoba nyoba

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGQmYgL4CcqGst6qdreAyv_fvEsR93iK4",
+  authDomain: "uas-pti-9e593.firebaseapp.com",
+  databaseURL: "https://uas-pti-9e593-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "uas-pti-9e593",
+  storageBucket: "uas-pti-9e593.firebasestorage.app",
+  messagingSenderId: "915369447558",
+  appId: "1:915369447558:web:b1171646bd20aa3ba24107",
+  measurementId: "G-F312T6E149"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
